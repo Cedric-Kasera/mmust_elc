68a1ac64ec8173176653b3bd8f9519b1c7c88274
Omar Kasera <omarkasera50@gmail.com>
2025-03-18 00:22:07 +0300

    Update .gitignore to exclude .env file and refactor firebase-config to use import.meta.env for environment variables.

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,18 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import dotenv from "dotenv";
-
-const process = dotenv.config();
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: import.meta.env.FIREBASE_API_KEY,
   authDomain: "mmust-elc.firebaseapp.com",
   projectId: "mmust-elc",
   storageBucket: "mmust-elc.firebasestorage.app",
-  messagingSenderId: process.env.FIREBASE_MESSENGER_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: import.meta.env.FIREBASE_MESSENGER_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
